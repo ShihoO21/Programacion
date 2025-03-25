d8a171b4aa7a118f0709e6911f207708943cf74c
ShihoO21 <sebastianparra2509@gmail.com>
2025-03-25 15:21:36 -0300

    Ultimos cambios

---
+++ variables/variables.py
@@ -0,0 +1,21 @@
+# booleano = True
+# bienvenida = "Bienvenida queridos estudiantes"
+
+# print(bienvenida)
+# del bienvenida
+
+# print(bienvenida)
+
+bienvenida = "Bienvenido"
+nombre = "Sebastian Parra"
+saludo = bienvenida + " " + nombre
+print(saludo)
+
+numero1 = 5
+numero2 = 7
+resultado= numero1 + numero2
+print(resultado)
+
+final = nombre + " " + str(numero2)
+print(final)
+
